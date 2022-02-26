d657dfea59cf4de510c9f9fcba124147d86b476b
azaveri879 <85413097+azaveri879@users.noreply.github.com>
2022-02-25 21:32:05 -0500

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Chat.css';
+import 'react-chat-elements/dist/main.css';
+import { StreamChat } from 'stream-chat';
+
+import  {ChatEngine } from 'react-chat-engine';
+
+
+
+function Chat() {
+    
+    return (
+        <ChatEngine
+            
+            projectID = "89083eb4-71b4-44a5-9f17-fcb51f4e2612"
+            userName= "Bob"
+            userSecret="123123"
+            
+          />
+    )
+}
+
+export default Chat
